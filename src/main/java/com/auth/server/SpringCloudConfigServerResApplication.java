8532d18f580cc0168f6c26ac7a897d23ddada8e0
Gyan Yadav <gyanicit@gmail.com>
2017-10-03 22:51:02 +0530

    Initial commit

---
+++ src/main/java/com/auth/server/SpringCloudConfigServerResApplication.java
@@ -0,0 +1,14 @@
+package com.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerResApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerResApplication.class, args);
+	}
+}
